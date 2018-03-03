f72f5e988f1c57757361279d2982f80b4739498a
thhuang <huangTzu06>
2018-03-03 23:57:28 +0800

    Update

---
+++ Course01/Backyard-Flyer-Project/README.md
@@ -0,0 +1,23 @@
+# FCND - Backyard Flyer Project
+
+## 1. Autonomously Flying
+
+```bash
+python backyard_flyer.py --route_size 10 --route_altitude 3
+```
+
+
+## 2. Manually Flying Trajectory
+
+```bash
+python plot_trajectory.py --logfile Logs/TLog-manual.txt --output Logs/trajectory_manually_flying.png
+```
+<div align='center'>
+<img src = 'Logs/trajectory_manually_flying.png' height="200px">
+</div>
+
+## 3. Autonomously Flying Trajectory
+
+```bash
+python plot_trajectory.py --logfile Logs/TLog.txt --output Logstrajectory_autonomously_flying.png
+```
--- Course01/Backyard-Flyer-Project/plot_trajectory.py
+++ Course01/Backyard-Flyer-Project/plot_trajectory.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from udacidrone import Drone
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--logfile', help='Path to the log file', type=str, default='Logs/TLog.txt')
+parser.add_argument('--logfile', help='Path to the log file', type=str, default='Logs/TLog-manual.txt')
+parser.add_argument('--output', help='Path to the output figure', type=str, default='Logs/trajectory_manually_flying.png')
 args = parser.parse_args()
 
 filename = args.logfile
@@ -12,8 +13,12 @@ t_log = Drone.read_telemetry_data(filename)
 
 # Time is always the first entry in the list
 time = t_log['MsgID.LOCAL_POSITION'][0][:]
-longitude = t_log['MsgID.LOCAL_POSITION'][1][:]
-latitude = t_log['MsgID.LOCAL_POSITION'][2][:]
+north = t_log['MsgID.LOCAL_POSITION'][1][:]
+east = t_log['MsgID.LOCAL_POSITION'][2][:]
 
-plt.plot(longitude, latitude)
-plt.show()
+plt.plot(north, east)
+plt.xlabel('North (m)')
+plt.ylabel('Ease (m)')
+plt.savefig(args.output)
+print('Plot of {} is written to {}'.format(args.logfile, args.output))
+plt.close()
