ea8c4fa9ebfdb0aa6cd33459f971e6a2bda532d5
thhuang <huangTzu06>
2018-03-04 23:46:25 +0800

    Upload

--- run_docker_druid.sh
+++ run_docker_druid.sh
@@ -4,7 +4,7 @@ PRJ_ROOT='/data/thhuang/fcnd'
 INPUT_PATH='/data/thhuang/fcnd_input'
 OUTPUT_PATH='/data/thhuang/fcnd_output'
 IMAGE_NAME='thhuang/fcnd'
-PORT='1234'
+PORT='8642'
 
 nvidia-docker run -ti -p ${PORT}:8765 \
                   -v ${PRJ_ROOT}:/app \
