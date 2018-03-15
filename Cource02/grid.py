b497cc36929574452dccf9c2a900007b879ad253
thhuang <huangTzu06>
2018-03-15 19:54:22 +0800

    Update

---
+++ Cource02/grid.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def create_grid(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space
+    based on given obstacle data, drone altitude and safety distance
+    arguments.
+    """
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.amin(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.amax(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.amin(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.amax(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil((north_max - north_min)))
+    east_size = int(np.ceil((east_max - east_min)))
+    # Initialize an empty grid
+    grid = np.zeros((north_size, east_size))
+    # Center offset for grid
+    north_min_center = np.min(data[:, 0])
+    east_min_center = np.min(data[:, 1])
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        # Determine which cells contain obstacles
+        # and set them to 1.
+        if drone_altitude > alt - d_alt - safety_distance and drone_altitude < alt + d_alt + safety_distance:
+            obstacle = [max(0,          int(np.floor(north - d_north - safety_distance - north_min))), 
+                        min(north_size, int(np.ceil( north + d_north + safety_distance - north_min))),
+                        max(0,          int(np.floor(east  - d_east  - safety_distance - east_min))),
+                        min(east_size,  int(np.ceil( east  + d_east  + safety_distance - east_min)))]
+            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
+
+    return grid
